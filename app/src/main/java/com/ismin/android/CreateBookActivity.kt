55961f22c1b9ddea13ee1c0d638c1f92c011bdaa
Bes Emilie <emilie.bes@etu.emse.fr>
2020-10-06 20:09:46 +0200

    Started implementation of a screen to add a book and then display it on main page

---
+++ app/src/main/java/com/ismin/android/CreateBookActivity.kt
@@ -0,0 +1,36 @@
+package com.ismin.android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import java.util.*
+
+class CreateBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_book)
+
+        val buttonSave = findViewById<Button>(R.id.buttonSave)
+        buttonSave.setOnClickListener{
+           val book = createBook();
+
+
+        }
+    }
+
+    fun createBook(): Book{
+        val edtTitle = findViewById<EditText>(R.id.editTextTitle);
+        val title = edtTitle.text.toString();
+
+        val edtAuthor = findViewById<EditText>(R.id.editTextAuthor);
+        val author = edtAuthor.text.toString();
+
+        val edtDate = findViewById<EditText>(R.id.editTextDate);
+        val date = edtDate.text.toString();
+
+        return(Book(title, author, date));
+    }
+}
